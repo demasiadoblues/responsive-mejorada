1269d68f773a062ce3a248c9d19e4cc917f13529
sebastian <manatiqa@outlook.com>
2024-05-14 13:34:40 -0300

    cambios copados
    agregue en la la pagina del formulario codigo para que pueda enviar el formulario sin necesidad de que me redireccione a la pagina formpree.ademas le agregue css porque se deformaba todo.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const buttonMenuToggle = document.querySelector('.button-menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
@@ -7,31 +5,58 @@ buttonMenuToggle.addEventListener('click', function() {
   navLinks.classList.toggle('active');
 });
 
-
 function showAlert() {
-    alert("¡Hola este es un alerta de javascript!")
+    alert("¡Hola, esta es una alerta de JavaScript!");
 }
 
-const form = document.getElementById("myForm")
-
 function validateEmail(email) {
-    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
-    return regex.test(email)
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
+    return regex.test(email);
 }
 
 function validateForm() {
-    const input = document.getElementById("email")
-    const inputValue = input.value
+    const input = document.getElementById("email");
+    const inputValue = input.value;
 
-
-    if(!validateEmail(inputValue)) {
-        alert("Por favor ingrese un correo electronico valido.")
-    }   else {
-        alert("Correo electronico enviado correctamente.")
+    if (!validateEmail(inputValue)) {
+        alert("Por favor ingrese un correo electrónico válido.");
+        return false; // Agregamos un return false para indicar que la validación falló
+    } else {
+        return true; // Indicamos que la validación fue exitosa
     }
-} 
+}
 
-form.addEventListener("submit", function(send) {
-    send.preventDefault()
-    validateForm();
-})
+const form = document.querySelector("form[name='myForm']");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevenir la acción por defecto del formulario
+
+    // Obtener los datos del formulario
+    const formData = new FormData(this);
+
+    // Enviar el formulario utilizando AJAX
+    fetch(this.action, {
+        method: this.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Correo electrónico enviado correctamente.");
+            form.reset(); // Reiniciar el formulario después de enviarlo
+        } else {
+            alert("Hubo un error al enviar el correo electrónico.");
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert("Hubo un error al enviar el correo electrónico.");
+    });
+});
+
+window.onbeforeunload = () => {
+    // Restablecer el formulario al cargar o actualizar la página
+    form.reset();
+};
